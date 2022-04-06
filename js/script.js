392c013b8948e44d06333efb2d21b58f606c185d
ryansyahreza142 <87295507+ryansyahreza142@users.noreply.github.com>
2022-04-06 14:53:28 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const txtElement = ['Web Developer']
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.lenght){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	setTimeout(ngetik, 100);
+
+})();
